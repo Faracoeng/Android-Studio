5c96bf0993491ba189ae97e5ca386831a043f36d
Faracoeng <alfmazucheli@gmail.com>
2019-11-19 08:12:54 -0300

    Criando primeiro metodo

--- AndroidStudioProjects/App_Teste/app/src/main/java/com/example/app_teste/MainActivity.java
+++ AndroidStudioProjects/App_Teste/app/src/main/java/com/example/app_teste/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -14,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG,"Aplicação iniciada");
+    }
+
+    public void alterarTexto(View view) {
     }
 }
