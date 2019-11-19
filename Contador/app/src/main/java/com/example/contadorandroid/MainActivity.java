130b062acb6dd165690059e05760f2647afd01ce
Faracoeng <alfmazucheli@gmail.com>
2019-11-19 09:09:12 -0300

    Contador finalizado

--- Contador/app/src/main/java/com/example/contadorandroid/MainActivity.java
+++ Contador/app/src/main/java/com/example/contadorandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.contadorandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int contador;
@@ -12,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.contador = 0;
+    }
+
+    public void decrementar(View view) {
+        // Decrementar contador
+        this.contador -= 1;
+        // Colocando mensagem na tela de saida
+        TextView mensagem = findViewById(R.id.telaContagem);
+        mensagem.setText(this.contador+"");
+    }
+
+    public void incrementar(View view) {
+        // Incrementar contador
+        this.contador += 1;
+        // Colocando mensagem na tela de saida
+        TextView mensagem = findViewById(R.id.telaContagem);
+        mensagem.setText(this.contador+"");
+    }
+
+    public void zerar(View view) {
+        // Incrementar contador
+        this.contador = 0;
+        // Colocando mensagem na tela de saida
+        TextView mensagem = findViewById(R.id.telaContagem);
+        mensagem.setText(this.contador+"");
     }
 }
