f6a662aefd754bfaacd356981ad62e4462f9bc66
Faracoeng <alfmazucheli@gmail.com>
2019-11-21 09:06:13 -0300

    comunicacao entre Activitys implementada

--- Comunicacao_Entre_Activitys/app/src/main/java/com/example/comunicacoesacitivitys/SegundaActivity.java
+++ Comunicacao_Entre_Activitys/app/src/main/java/com/example/comunicacoesacitivitys/SegundaActivity.java
@@ -2,7 +2,10 @@ package com.example.comunicacoesacitivitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class SegundaActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class SegundaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+    }
+
+    public void botao01(View view) {
+//        Uri uri = Uri.parse("https://www.google.com/");
+//        Intent it = new Intent(Intent.ACTION_VIEW,uri);
+//        startActivity(it);
+        Intent menssagemIntent = new Intent(this,MainActivity.class);
+        startActivity(menssagemIntent);
+    }
+
+    public void botao03(View view) {
+//        Uri uri = Uri.parse("tel:3381800");
+//        Intent it = new Intent(Intent.ACTION_DIAL,uri);
+//        startActivity(it);
+        Intent menssagemIntent = new Intent(this,TerceiraActivity.class);
+        startActivity(menssagemIntent);
+
     }
 }
--- Comunicacao_Entre_Activitys/app/src/main/java/com/example/comunicacoesacitivitys/TerceiraActivity.java
+++ Comunicacao_Entre_Activitys/app/src/main/java/com/example/comunicacoesacitivitys/TerceiraActivity.java
@@ -2,7 +2,9 @@ package com.example.comunicacoesacitivitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TerceiraActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class TerceiraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terceira);
+    }
+
+    public void botao01(View view) {
+        Intent menssagemIntent = new Intent(this,MainActivity.class);
+        startActivity(menssagemIntent);
+    }
+
+    public void botao03(View view) {
+        Intent menssagemIntent = new Intent(this,TerceiraActivity.class);
+        startActivity(menssagemIntent);
     }
 }
