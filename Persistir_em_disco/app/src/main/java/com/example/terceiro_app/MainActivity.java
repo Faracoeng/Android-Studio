5623888773d72dfa3be4101d498019ce0cd569ce
Faracoeng <alfmazucheli@gmail.com>
2019-11-28 09:04:01 -0300

    imagens add

--- Persistir_em_disco/app/src/main/java/com/example/terceiro_app/MainActivity.java
+++ Persistir_em_disco/app/src/main/java/com/example/terceiro_app/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String sSharadPreFile = "br.edu.ifsc.sj.minhaspreferencias";
     private SharedPreferences mSharedPreferences;
-    private int mContador = 0;
+    private int mContador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +19,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.mSharedPreferences = getSharedPreferences(sSharadPreFile,MODE_PRIVATE);
-        mContador = 0;
+        // mContador = 0;
+        mContador = mSharedPreferences.getInt("contador",0);
         TextView textView = findViewById(R.id.contador);
         textView.setText(""+mContador);
     }
 
+    // incrementando a textView
     public void decrementar(View view) {
+
         this.mContador--;
         TextView textView = findViewById(R.id.contador);
         textView.setText(""+mContador);
+
+
     }
+
+    // decrementando a textView
 
     public void incrementar(View view) {
         this.mContador++;
+        TextView textView = findViewById(R.id.contador);
+        textView.setText(""+mContador);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor prefsEditor = mSharedPreferences.edit();
+
+        prefsEditor.putInt("contador", mContador); // HasMap, utilizar no proj03 para armazenar placar, nome do jogador  e tempo , ou dificuldade
+
+        prefsEditor.apply();
+
+    }
+
+    public void zerar(View view) {
+
+        SharedPreferences.Editor prefsEditor = mSharedPreferences.edit();
+        prefsEditor.clear();
+        prefsEditor.apply();
+
+        this.mContador = 0;
+
         TextView textView = findViewById(R.id.contador);
         textView.setText(""+mContador);
     }
