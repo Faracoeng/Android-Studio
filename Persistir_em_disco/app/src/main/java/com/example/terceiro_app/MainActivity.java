2aca26debbba5ff97f607cce27094f4b07cfc097
Faracoeng <alfmazucheli@gmail.com>
2019-11-28 08:21:50 -0300

    persistir em disco, implementado contador

--- Persistir_em_disco/app/src/main/java/com/example/terceiro_app/MainActivity.java
+++ Persistir_em_disco/app/src/main/java/com/example/terceiro_app/MainActivity.java
@@ -4,15 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String sSharadPreFile = "br.edu.ifsc.sj.minhaspreferencias";
     private SharedPreferences mSharedPreferences;
+    private int mContador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mSharedPreferences = getSharedPreferences(sSharadPreFile,MODE_PRIVATE);
+        mContador = 0;
+        TextView textView = findViewById(R.id.contador);
+        textView.setText(""+mContador);
+    }
+
+    public void decrementar(View view) {
+        this.mContador--;
+        TextView textView = findViewById(R.id.contador);
+        textView.setText(""+mContador);
+    }
+
+    public void incrementar(View view) {
+        this.mContador++;
+        TextView textView = findViewById(R.id.contador);
+        textView.setText(""+mContador);
     }
 }
