16c04b9ee1f505205a778ffa12f0b94f73522ba3
Faracoeng <alfmazucheli@gmail.com>
2019-12-19 09:59:35 -0300

    Activity genius iniciada

---
+++ Genius/app/src/main/java/com/example/genius/GeniusActivity.java
@@ -0,0 +1,14 @@
+package com.example.genius;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeniusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genius);
+    }
+}
