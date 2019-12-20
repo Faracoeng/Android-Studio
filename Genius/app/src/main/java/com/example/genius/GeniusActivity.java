4e1e61c591acdaeab45d85366668353990f11af4
Faracoeng <alfmazucheli@gmail.com>
2019-12-20 08:58:26 -0300

    gerando sequencia aleatoria

--- Genius/app/src/main/java/com/example/genius/GeniusActivity.java
+++ Genius/app/src/main/java/com/example/genius/GeniusActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class GeniusActivity extends AppCompatActivity {
 
     public final static int VALOR_REQUEST = 3;
@@ -21,8 +23,8 @@ public class GeniusActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         this.nivelDeDifiuldade = extras.getInt("dificuldade");
-        TextView view = findViewById(R.id.mostrarDificuldade);
-        view.setText(this.nivelDeDifiuldade + " rodadas");
+        //TextView view = findViewById(R.id.mostrarDificuldade);
+        //view.setText(this.nivelDeDifiuldade + " rodadas");
 
         if (extras != null) {
             //this.nivelDeDifiuldade = 20;
@@ -34,10 +36,19 @@ public class GeniusActivity extends AppCompatActivity {
             this.sequenciaAleatoria = new int[this.nivelDeDifiuldade];
             this.gerarSequenciaAleatoria(this.nivelDeDifiuldade);
 // ----------------------------acender botoes indicando inicio do jogo------------------------------
-            this.trocaImagem(R.id.vermelhor_apagado, R.drawable.ic_vermelhorapagado, R.drawable.ic_vermelhoraceso);
-            this.trocaImagem(R.id.azul_apagado, R.drawable.ic_azulapagado, R.drawable.ic_azulaceso);
-            this.trocaImagem(R.id.verde_apagado, R.drawable.ic_verdeapagado, R.drawable.ic_verdeaceso);
-            this.trocaImagem(R.id.amarelo_apagado, R.drawable.ic_amareloapagado, R.drawable.ic_amareloaceso);
+//            this.trocaImagem(R.id.vermelhor_apagado, R.drawable.ic_vermelhorapagado, R.drawable.ic_vermelhoraceso);
+//            this.trocaImagem(R.id.azul_apagado, R.drawable.ic_azulapagado, R.drawable.ic_azulaceso);
+//            this.trocaImagem(R.id.verde_apagado, R.drawable.ic_verdeapagado, R.drawable.ic_verdeaceso);
+//            this.trocaImagem(R.id.amarelo_apagado, R.drawable.ic_amareloapagado, R.drawable.ic_amareloaceso);
+        }
+    }
+//-------------------Gerar sequencia aleatoria em vetor---------------------------------------------
+    private void gerarSequenciaAleatoria(int tamanho) {
+        Random gerador = new Random();
+
+        for (int i = 0; i < tamanho; i++) {
+            this.sequenciaAleatoria[i] = gerador.nextInt(4); //ok debugado, esta funcionando
+            // corretamente
         }
     }
 
